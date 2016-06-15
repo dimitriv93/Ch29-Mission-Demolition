10d467acfcdee0bc2aa95a5a211e663ace22eb04
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-15 18:58:16 -0400

    Returning for Another Shot
    update script so the camera resets after projectile is at rest

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -19,9 +19,24 @@ public class FollowCam : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		// if there's only one line following an if, it doesn't need braces
-		if (poi == null) return; // return if there is no poi
-		// Get the position of the poi
-		Vector3 destination = poi.transform.position;
+		Vector3 destination;
+		if (poi == null) {
+			destination = Vector3.zero;
+		} else {
+			// Get the position of the poi
+			destination = poi.transform.position;
+			// If poi is a Projectile, check to see if it's at rest
+			if (poi.tag == "Projectile") {
+				// if it is sleeping (that is, not moving)
+				if ( poi.rigidbody.IsSleeping() ) {
+					// return to default view
+					poi = null;
+					// in the next update
+					return;
+				}
+			}
+		}
+		//
 		// Limit the X & Y to minimum values
 		destination.x = Mathf.Max( minXY.x, destination.x );
 		destination.y = Mathf.Max( minXY.y, destination.y );
