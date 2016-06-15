1deff58afc20997e3f550e3704b25ae26f9f7004
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-15 16:06:35 -0400

    Slingshot script
    Creating script that shows halo when the singshot is active

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+	void Awake() {
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive( false );
+	}
+
+	void OnMouseEnter() {
+		//print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive( true );
+	}
+	void OnMouseExit() {
+		//print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive( false );
+	}
+}
