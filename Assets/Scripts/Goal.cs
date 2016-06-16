0ba2c4537138f2021bce1c06ba7b134191fd5ab0
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-15 23:34:17 -0400

    Adding More Levels and Game Logic
    Creating more levels and dui text for shots taken

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Goal : MonoBehaviour {
 	// A static field accessible by code anywhere
+	// A static field accessible by code anywhere
 	static public bool goalMet = false;
 	void OnTriggerEnter( Collider other ) {
 		// When the trigger is hit by something
